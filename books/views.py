9130a9d0628e313c2d9ff34ef048ab8a6a9b73c6
ja2001 <ja.2001@yandex.ru>
2024-10-23 18:07:38 +0300

    POST+GET

--- books/views.py
+++ books/views.py
@@ -6,15 +6,28 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Главная")
 
-class UnitListView(generics.ListAPIView):
+class UnitListView(generics.ListAPIView, generics.CreateAPIView):
     
     serializer_class = BookSerializer
 
-    def get(self, request, id:int):
-        queryset = Book.objects.filter(id=id)
-        serializer = self.get_serializer(queryset, many = True)
-
+    def get(self, request): #Получение список всех книг (GET)
+        author = request.GET.get("author")
+        published_date = request.GET.get("date")
+        if author:
+            queryset = Book.objects.filter(author=author)
+            # serializer = BookSerializer(queryset.first())
+        elif published_date:
+            queryset = Book.objects.filter(published_date=published_date)
+        else:
+            queryset = Book.objects.all()
+        
+        serializer = self.get_serializer(queryset, many = True)    
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            book = serializer.save()
+            return Response(BookSerializer(book).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- library/urls.py
+++ library/urls.py
@@ -2,6 +2,6 @@ from books import views
 from django.urls import path, re_path
 
 urlpatterns = [
-    path('', views.index),
-    re_path(r"book/(?P<id>\d+)/$", views.UnitListView.as_view()),
+    # path('',  views.UnitListView.as_view()),
+    re_path(r"book/$", views.UnitListView.as_view()),
 ]
